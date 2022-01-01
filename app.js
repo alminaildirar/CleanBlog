691b19bc86e9674e1cc9d0a8372199f939787985
alminaildirar <alminaildirar@gmail.com>
2022-01-01 22:30:23 +0300

    mongoose, CRUD

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const mongoose =require('mongoose')
+const ejs = require('ejs')
+const path = require('path');
+const Blog = require('./models/Blog.js')
+
+
 const app = express()
 
+//connection database
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true    
+})
+
+
+
+//Middlewares
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.get('/', (req,res) => {
- res.render('index')
+
+//Routers
+app.get('/', async(req,res) => {
+const blogs =  await Blog.find({})
+ res.render('index', {
+     blogs
+ })
 })
 app.get('/about', (req,res) => {
     res.render('about')
@@ -13,6 +35,12 @@ app.get('/about', (req,res) => {
 app.get('/add_post', (req,res) => {
  res.render('add_post')
 })
+app.post('/blogs',async(req,res)=> {
+    await Blog.create(req.body)
+    res.redirect('/')
+})
+
+
 
 const port = 3000;
 app.listen(port, () => {
